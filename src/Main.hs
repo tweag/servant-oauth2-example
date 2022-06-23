e57b7ed21d6e15895cc36bc333a0f1b55fabea45
Noon van der Silk <noon.vandersilk@tweag.io>
2022-06-23 08:57:17 +0100

    auth!

--- src/Main.hs
+++ src/Main.hs
@@ -1,34 +1,60 @@
-{-# language TemplateHaskell #-}
 {-# language QuasiQuotes     #-}
+{-# language TemplateHaskell #-}
+{-# language TypeFamilies    #-}
 
 module Main where
 
-import Network.Wai.Handler.Warp (run)
-import Servant                  (Handler, type (:>), Proxy, Get, Context((:.), EmptyContext))
-import Servant.API.Generic      ((:-))
-import Servant.HTML.Blaze       (HTML)
-import Servant.Server.Generic   (AsServerT, genericServeTWithContext)
-import Text.Hamlet              (Html, shamlet)
-import GHC.Generics             (Generic)
+import Data.Text                        (Text)
+import Network.Wai.Handler.Warp         (run)
+import Servant                          (Handler, type (:>), Get, Context((:.), EmptyContext)
+                                        , NoContent, Header, Headers, WithStatus, UVerb, StdMethod(GET)
+                                        , AuthProtect, NamedRoutes)
+import Servant.API.Generic              ((:-))
+import Servant.HTML.Blaze               (HTML)
+import Servant.Server.Generic           (AsServerT, genericServeTWithContext)
+import Text.Hamlet                      (Html, shamlet)
+import GHC.Generics                     (Generic)
+import Servant.Server.Experimental.Auth (AuthServerData, AuthHandler)
+import Network.Wai                      (Request)
 
 type PageM = Handler
 
 data Api mode = Api
   { home  :: mode :- Get '[HTML] Html
   , about :: mode :- "about" :> Get '[HTML] Html
+  , auth :: mode :- "auth" :> "github" :> NamedRoutes AuthRoutes
   }
   deriving stock Generic
 
+data Login = Login
+
+data AuthRoutes mode = AuthRoutes
+  { login :: mode :- AuthProtect "login" :> "login" :> UVerb 'GET '[HTML] '[ WithStatus 301 (Headers '[ Header "Location" Text ] NoContent) ]
+  }
+  deriving stock Generic
+
+type instance AuthServerData (AuthProtect "login") = Login
+
+
 server :: Api (AsServerT PageM)
 server = Api
-  { home = pure $ [shamlet| <p> Home |]
+  { home  = pure $ [shamlet| <p> Home |]
   , about = pure $ [shamlet| <p> About |]
+  , auth  = authServer
   }
+
+authServer :: AuthRoutes (AsServerT PageM)
+authServer = AuthRoutes
+  { login = \Login -> undefined
+  }
+
+loginContext :: AuthHandler Request Login
+loginContext = undefined
 
 main :: IO ()
 main = do
   run 8083 $
     genericServeTWithContext nat server context
       where
-        context = EmptyContext
+        context = loginContext :. EmptyContext
         nat = id
